5abd11373db1f4a7f02ca3cfac94ed76a2c3b047
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-12-18 17:55:54 +0900

    Updates

---
+++ precache-manifest.b0cad9af3b9668b9161fce633715c097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da9f75fbabd4c3a0813",
+    "url": "./static/css/main.da3333f7.chunk.css"
+  },
+  {
+    "revision": "4da9f75fbabd4c3a0813",
+    "url": "./static/js/main.4da9f75f.chunk.js"
+  },
+  {
+    "revision": "aa7f47607dfc17847eda",
+    "url": "./static/js/1.aa7f4760.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4b740c2f5df4b44d9eb90682e29811a7",
+    "url": "./index.html"
+  }
+];
